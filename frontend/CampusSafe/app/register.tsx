5217e714d5b7dc9c8eb3d9144d0bd8b0af7d79f2
JosephBuchholz <joseph.buchholz@outlook.com>
2024-10-21 20:13:46 -0700

    Added basic navigation

---
+++ frontend/CampusSafe/app/register.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function RegisterScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Register Screen</Text>
+    </View>
+  );
+}

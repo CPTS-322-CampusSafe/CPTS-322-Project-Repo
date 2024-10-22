4098849c9dd3c4af8febca1568b76b6f2943c2d6
JosephBuchholz <70069846+JosephBuchholz@users.noreply.github.com>
2024-10-22 15:35:53 -0700

    Added a get profile api request

--- backend/campussafe/auth_api/views.py
+++ backend/campussafe/auth_api/views.py
@@ -26,12 +26,37 @@ def login_view(request):
 def is_logged_in_view(request):
     """
     Checks whether the requester is logged in.
-    Will return their username if they are logged in, and
-    will return an empty string if they are not logged in.
     """
 
-    username = request.user.username
+    if request.user.is_authenticated:
+        username = request.user.username
 
-    return Response({
-        "username": username,
-    })
+        return Response()
+    else:
+        return Response("Not logged in", status=status.HTTP_401_UNAUTHORIZED)
+
+@api_view(["GET"])
+def get_profile_view(request):
+    """
+    Gets the user's information including profile.
+    """
+
+    if request.user.is_authenticated:
+        profile = {
+            "username": request.user.username,
+            "first_name": request.user.first_name,
+            "last_name": request.user.last_name,
+            "email": request.user.email,
+            "date_registered": request.user.date_joined,
+            "phone_number": request.user.profile.phone_number.as_e164,
+            "is_user_admin": request.user.profile.is_user_admin,
+            "settings": {
+                "recieve_email_notifications": request.user.profile.user_settings.recieve_email_notifications,
+                "recieve_SMS_notifications": request.user.profile.user_settings.recieve_SMS_notifications,
+                "recieve_in_app_notifications": request.user.profile.user_settings.recieve_in_app_notifications,
+            }
+        }
+
+        return Response(profile)
+    else:
+        return Response("Not logged in", status=status.HTTP_401_UNAUTHORIZED)

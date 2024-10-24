6d7b4c9820b7d30aacd99d207dd19bfb8769496d
Joseph Buchholz <70069846+JosephBuchholz@users.noreply.github.com>
2024-10-24 11:32:11 -0700

    Started adding register api

--- backend/campussafe/auth_api/views.py
+++ backend/campussafe/auth_api/views.py
@@ -3,6 +3,43 @@ from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.response import Response
 from django.contrib.auth import authenticate, login, logout
+from .serializers import UserSerializer
+from .models import Profile, UserSettings
+from phonenumber_field.validators import validate_international_phonenumber
+
+@api_view(["POST"])
+def register_view(request):
+    """
+    Creates a new account for a user.
+    """
+
+    # Make sure phone_number field exists
+    try:
+        phone_number = request.data["phone_number"]
+    except:
+        return Response("Missing phone number", status=status.HTTP_400_BAD_REQUEST)
+
+    # Make sure the phone number is a valid phone number
+    try:
+        validate_international_phonenumber(phone_number)
+    except:
+        return Response("Invalid phone number", status=status.HTTP_400_BAD_REQUEST)
+
+    # Make sure a user_data section is present
+    try:
+        user_data = request.data["user_data"]
+    except:
+        return Response("Missing important information", status=status.HTTP_400_BAD_REQUEST)
+
+    # Serialize the user_data and other information
+    serializer = UserSerializer(data=user_data)
+    if serializer.is_valid():
+        user = serializer.save() # Create user
+        user_settings = UserSettings.objects.create() # Create user settings
+        profile = Profile.objects.create(user=user,user_settings=user_settings,phone_number=phone_number) # Create user profile
+        return Response("Successfully registered")
+    else:
+        return Response("Failed to register", status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(["POST"])
 def login_view(request):

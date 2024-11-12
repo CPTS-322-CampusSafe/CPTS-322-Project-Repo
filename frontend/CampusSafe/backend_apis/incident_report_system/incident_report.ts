61d4526f15ebe635788073ff3c5ce955f911c546
JosephBuchholz <70069846+JosephBuchholz@users.noreply.github.com>
2024-11-11 16:32:27 -0800

    Create incident reports from frontend

--- frontend/CampusSafe/backend_apis/incident_report_system/incident_report.ts
+++ frontend/CampusSafe/backend_apis/incident_report_system/incident_report.ts
@@ -1,12 +1,26 @@
 export default class IncidentReport {
+    // These are read only and are not sent to the server:
     id: number = 0;
+    isVerified: boolean = false;
+    isResolved: boolean = false;
+    recievedAt: Date = new Date(); // When this report was recieved by the server
+    updatedAt: Date = new Date(); // The last time this report was updated
+    verifiedAt: Date = new Date(); // The time this report was verified by an admin
+    resolvedAt: Date = new Date(); // The time this report was marked as resolved
+
+    // These will be sent to the server:
     title: string = "";
     summary: string = "";
     description: string = "";
-    isVerified: boolean = false;
-    isResolved: boolean = false;
     location: string = "";
+
+    /**
+     * 0-5: non emergencies
+     * 5-10: emergencies (should send notification)
+     */
     emergencyLevel: number = 0;
+
+    happenedAt: Date = new Date(); // The time that the user said this incident happened
 
     deserialize(json: any) {
         this.id = json.id;
@@ -17,5 +31,11 @@ export default class IncidentReport {
         this.isResolved = json.is_resolved;
         this.location = json.location;
         this.emergencyLevel = json.emergency_level;
+
+        this.recievedAt = new Date(json.recieved_at);
+        this.updatedAt = new Date(json.updated_at);
+        this.verifiedAt = new Date(json.verified_at);
+        this.resolvedAt = new Date(json.resolved_at);
+        this.happenedAt = new Date(json.happended_at);
     }
 }
